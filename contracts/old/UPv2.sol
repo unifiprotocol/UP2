19834f9cfc37b42aaaf41c298c7b51ede6d53831
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-05-10 18:46:19 +0200

    chore: restored orig UPv2.sol

--- contracts/old/UPv2.sol
+++ contracts/old/UPv2.sol
@@ -196,11 +196,57 @@ contract UPv2 is MinterRole {
   }
 
   /**
+   * @dev Burns a specific amount of tokens and return the backed value
+   * @param value The amount of token to be burned.
+   */
+  function burn(uint256 value) public {
+    _burn(msg.sender, value);
+  }
+
+  /**
+   * @dev Burns a specific amount of tokens.
+   * @param value The amount of token to be burned.
+   */
+  function justBurn(uint256 value) public {
+    _justBurn(msg.sender, value);
+  }
+
+  /**
    * @dev Deposits a number of native token to return debt
    * @param value The amount of token to be burned.
    */
   function justDeposit(uint256 value) public payable {
     _justDeposit(msg.sender, value);
+  }
+
+  /**
+   * @dev Burns a specific amount of tokens from the target address and decrements allowance
+   * @param from address The address which you want to send tokens from
+   * @param value uint256 The amount of token to be burned
+   */
+  function burnFrom(address from, uint256 value) public {
+    _burnFrom(from, value);
+  }
+
+  /**
+   * @return the name of the token.
+   */
+  function name() public view returns (string memory) {
+    return _name;
+  }
+
+  /**
+   * @return the symbol of the token.
+   */
+  function symbol() public view returns (string memory) {
+    return _symbol;
+  }
+
+  /**
+   * @return the number of decimals of the token.
+   */
+  function decimals() public view returns (uint8) {
+    return _decimals;
   }
 
   /**
@@ -301,6 +347,15 @@ contract UPv2 is MinterRole {
   }
 
   /**
+   * @dev Gets the balance of the specified address.
+   * @param _account The address to query the balance of.
+   * @return An uint256 representing the amount owned by the passed address.
+   */
+  function balanceOf(address _account) public view returns (uint256) {
+    return _balances[_account];
+  }
+
+  /**
    * @dev Function to return total number of native toekns given out by redeem.
    */
   function getTotalFeesGiven() public view returns (uint256) {
@@ -312,6 +367,120 @@ contract UPv2 is MinterRole {
    */
   function getTotalUPBurnt() public view returns (uint256) {
     return totalUPBurnt;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param owner address The address which owns the funds.
+   * @param spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(address owner, address spender) public view returns (uint256) {
+    return _allowed[owner][spender];
+  }
+
+  /**
+   * @dev Transfer token for a specified address
+   * @param to The address to transfer to.
+   * @param value The amount to be transferred.
+   */
+  function transfer(address to, uint256 value) public returns (bool) {
+    _transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function transferMulti(address[] memory to, uint256[] memory value) public returns (bool) {
+    uint256 i = 0;
+    while (i < to.length) {
+      _transfer(msg.sender, to[i], value[i]);
+      i++;
+    }
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param spender The address which will spend the funds.
+   * @param value The amount of tokens to be spent.
+   */
+  function approve(address spender, uint256 value) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param from address The address which you want to send tokens from
+   * @param to address The address which you want to transfer to
+   * @param value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  ) public returns (bool) {
+    _allowed[from][msg.sender] = _allowed[from][msg.sender] - (value);
+    _transfer(from, to, value);
+    return true;
+  }
+
+  /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param spender The address which will spend the funds.
+   * @param addedValue The amount of tokens to increase the allowance by.
+   */
+  function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (_allowed[msg.sender][spender] + (addedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param spender The address which will spend the funds.
+   * @param subtractedValue The amount of tokens to decrease the allowance by.
+   */
+  function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (_allowed[msg.sender][spender] - (subtractedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  /**
+   * @dev Transfer token for a specified addresses
+   * @param from The address to transfer from.
+   * @param to The address to transfer to.
+   * @param value The amount to be transferred.
+   */
+  function _transfer(
+    address from,
+    address to,
+    uint256 value
+  ) internal {
+    require(to != address(0));
+
+    _balances[from] = _balances[from] - (value);
+    _balances[to] = _balances[to] + (value);
+    emit Transfer(from, to, value);
   }
 
   /**
@@ -342,6 +511,19 @@ contract UPv2 is MinterRole {
     totalUPBurnt = totalUPBurnt + (value);
     totalFeesGiven = totalFeesGiven + (redeemValue);
     payable(address(account)).transfer(redeemValue);
+    _totalSupply = _totalSupply - (value);
+    _balances[account] = _balances[account] - (value);
+    emit Transfer(account, address(0), value);
+  }
+
+  /**
+   * @dev Internal function that burns an amount of the token of a given
+   * account.
+   * @param account The account whose tokens will be burnt.
+   * @param value The amount that will be burnt.
+   */
+  function _justBurn(address account, uint256 value) internal {
+    require(account != address(0));
     _totalSupply = _totalSupply - (value);
     _balances[account] = _balances[account] - (value);
     emit Transfer(account, address(0), value);
