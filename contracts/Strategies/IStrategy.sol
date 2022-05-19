cf07341ef8dfb64dc0a3d378edb276e470686a06
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-05-19 09:31:30 +0200

    doc: IStrategy

--- contracts/Strategies/IStrategy.sol
+++ contracts/Strategies/IStrategy.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.4;
 
 interface IStrategy {
+  /// @notice Deposits an initial or more liquidity in the external contract
   function deposit(uint256 amount) external returns (bool);
 
+  /// @notice Withdraws all the funds deposited in the external contract
   function withdraw(uint256 amount) external returns (bool);
 
+  /// @notice This function will get all the rewards from the external service and send them to the invoker
   function gather() external;
 }
