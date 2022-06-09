9e6d1532d427dfb1ff97f0a1e8ef292eec9e5a05
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-06-09 15:49:10 -0500

    feat: Darbi updates

--- contracts/Darbi/Darbi.sol
+++ contracts/Darbi/Darbi.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.4;
 
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IWETH.sol";
-import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@uniswap/lib/contracts/libraries/Babylonian.sol";
 import "../Libraries/UniswapHelper.sol";
 import "../Helpers/Safe.sol";
@@ -16,11 +14,10 @@ import "./UPMintDarbi.sol";
 
 contract Darbi is AccessControl, Safe {
   using SafeERC20 for IERC20;
-  using SafeMath for uint256;
 
   address public factory;
+  address public WETH;
   IUniswapV2Router02 public router;
-  IWETH public WETH;
   UPController public UP_CONTROLLER;
   UPMintDarbi public DARBI_MINTER;
 
@@ -38,84 +35,79 @@ contract Darbi is AccessControl, Safe {
   ) {
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     factory = _factory;
+    WETH = _WETH;
     router = IUniswapV2Router02(_router);
-    WETH = IWETH(_WETH);
     UP_CONTROLLER = UPController(payable(_UP_CONTROLLER));
     DARBI_MINTER = UPMintDarbi(payable(_darbiMinter));
   }
 
-  receive() external payable {
-    WETH.deposit{value: msg.value}();
-  }
+  receive() external payable {}
 
   function arbitrage() public onlyAdmin {
-    uint256 balance1 = IERC20(address(WETH)).balanceOf(address(this));
-    uint256 price1 = UP_CONTROLLER.getVirtualPrice();
-    uint256 balance0 = price1 * balance1;
-    uint256 price0 = UniswapV2Library.quote(1, balance0, balance1);
     address UP_TOKEN = UP_CONTROLLER.UP_TOKEN();
+    (uint256 reserves0, uint256 reserves1) = UniswapHelper.getReserves(factory, UP_TOKEN, WETH);
+    // uint256 marketValue = reserves0 / reserves1;
+    uint256 backedValue = UP_CONTROLLER.getVirtualPrice();
+    address[] memory path;
 
-    address[2] memory swappedTokens = [UP_TOKEN, address(WETH)];
-    uint256[2] memory truePriceTokens = [price0, price1];
-    uint256[2] memory maxSpendTokens = [balance0, balance1];
+    (bool aToB, uint256 amountIn) = moveMarketBuyAmount();
+    // Will Return 0 if MV = BV exactly
+    if (amountIn == 0) return;
 
-    require(truePriceTokens[0] != 0 && truePriceTokens[1] != 0, "SwapToPrice: ZERO_PRICE");
-    // caller can specify 0 for either if they wish to swap in only one direction, but not both
-    require(maxSpendTokens[0] != 0 || maxSpendTokens[1] != 0, "SwapToPrice: ZERO_SPEND");
+    // aToB == true == Buys UP
+    // aToB == fals == Sells UP
 
-    bool aToB;
-    uint256 amountIn;
-
-    (uint256 reserveA, uint256 reserveB) = UniswapHelper.getReserves(
-      factory,
-      swappedTokens[0],
-      swappedTokens[1]
-    );
-    (aToB, amountIn) = UniswapHelper.computeTradeToMoveMarket(
-      truePriceTokens[0],
-      truePriceTokens[1],
-      reserveA,
-      reserveB
-    );
-
-    require(amountIn > 0, "SwapToPrice: ZERO_AMOUNT_IN");
-
-    // spend up to the allowance of the token in
-    uint256 maxSpend = aToB ? maxSpendTokens[0] : maxSpendTokens[1];
-
-    // IF ATOB === TRUE WE HAVE TO GET MINT $UP !!!
+    uint256 balances = address(this).balance;
+    uint256 actualAmountIn;
+    // If Buying UP
     if (aToB) {
-      mintUP(maxSpend);
-    }
+      actualAmountIn = amountIn <= balances ? amountIn : balances; //Value is going to native
+      uint256 expectedReturn = UniswapHelper.getAmountOut(actualAmountIn, reserves0, reserves1); // Amount of UP expected from Buy
+      uint256 expectedNativeReturn = expectedReturn * backedValue; //Amount of Native Tokens Expected to Receive from Redeem
+      uint256 upControllerBalance = address(UP_CONTROLLER).balance;
+      if (upControllerBalance < expectedNativeReturn) {
+        uint256 upOutput = upControllerBalance / backedValue; //Value in UP Token
+        actualAmountIn = UniswapHelper.getAmountOut(upOutput, reserves1, reserves0); // Amount of UP expected from Buy
+      }
+      path[0] = WETH;
+      path[1] = UP_TOKEN;
+      uint256[] memory amounts = router.swapExactETHForTokens(
+        actualAmountIn,
+        path,
+        address(this),
+        block.timestamp + 20 minutes
+      );
+      UP_CONTROLLER.redeem(amounts[1]);
+      // Gas Refund HERE
+      uint256 diffBalances = address(this).balance - balances;
+      (bool success, ) = address(UP_CONTROLLER).call{value: diffBalances}("");
+      require(success, "FAIL_SENDING_BALANCES_TO_CONTROLLER");
+    } else {
+      uint256 darbiBalanceMaximumUpToMint = balances / backedValue;
+      actualAmountIn = amountIn <= darbiBalanceMaximumUpToMint
+        ? amountIn
+        : darbiBalanceMaximumUpToMint; // Value in UP
+      uint256 nativeToMint = actualAmountIn / backedValue;
+      DARBI_MINTER.mintUP{value: nativeToMint}();
 
-    if (amountIn > maxSpend) {
-      amountIn = maxSpend;
-    }
-
-    address tokenIn = aToB ? swappedTokens[0] : swappedTokens[1];
-    address tokenOut = aToB ? swappedTokens[1] : swappedTokens[0];
-
-    TransferHelper.safeApprove(tokenIn, address(router), amountIn);
-
-    address[] memory path = new address[](2);
-    path[0] = tokenIn;
-    path[1] = tokenOut;
-
-    router.swapExactTokensForTokens(
-      amountIn,
-      0, // amountOutMin: we can skip computing this number because the math is tested within the uniswap tests.
-      path,
-      address(this),
-      1 minutes
-    );
-
-    // IF ATOB === TRUE WE ARE GETTING $UP, SO WE HAVE TO REDEEM IT
-    if (aToB) {
-      redeemUP();
+      path[0] = UP_TOKEN;
+      path[1] = WETH;
+      router.swapExactTokensForETH(
+        IERC20(UP_TOKEN).balanceOf(address(this)),
+        0,
+        path,
+        address(this),
+        block.timestamp + 20 minutes
+      );
+      uint256 diffBalances = address(this).balance - balances;
+      (bool success, ) = address(UP_CONTROLLER).call{value: diffBalances}("");
+      require(success, "FAIL_SENDING_BALANCES_TO_CONTROLLER");
+      //This Sells UP
     }
   }
 
-  function moveMarketBuyAmount() public returns (bool aToB, uint256 amountIn) {
+  function moveMarketBuyAmount() public view returns (bool aToB, uint256 amountIn) {
+    address UP_TOKEN = UP_CONTROLLER.UP_TOKEN();
     address[2] memory swappedTokens = [UP_TOKEN, address(WETH)];
     (uint256 reserveA, uint256 reserveB) = UniswapHelper.getReserves(
       factory,
@@ -127,22 +119,18 @@ contract Darbi is AccessControl, Safe {
     return (aToB, amountIn);
   }
 
-  function getVirtualPrice() public returns (uint256, uint256) {
-    uint256 price1 = UP_CONTROLLER.getVirtualPrice();
-    uint256 balance0 = price1 * balance1;
-    uint256 price0 = UniswapV2Library.quote(1, balance0, balance1);
-    address UP_TOKEN = UP_CONTROLLER.UP_TOKEN();
+  function getVirtualPrice() public view returns (uint256, uint256) {
+    uint256 price0 = UP_CONTROLLER.getVirtualPrice();
+    uint256 price1 = 1e18 / price0;
     return (price0, price1);
   }
 
   function mintUP(uint256 amount) internal {
-    WETH.withdraw(amount);
     DARBI_MINTER.mintUP{value: amount}();
   }
 
   function redeemUP() internal {
     UP_CONTROLLER.redeem(IERC20(UP_CONTROLLER.UP_TOKEN()).balanceOf(address(this)));
-    WETH.deposit{value: address(this).balance}();
   }
 
   function setFactory(address _factory) public onlyAdmin {
