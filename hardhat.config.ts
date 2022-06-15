4e432ad9a12d1e223aa2fab3350d4303e6ae54ce
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-06-15 10:13:05 +0200

    feat: UPMintPublic task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,21 +5,13 @@ import "@typechain/hardhat"
 import "@nomiclabs/hardhat-ethers"
 import "solidity-docgen"
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(await account.address)
-  }
-})
-
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+// TASKS
+import "./scripts/UPMintPublic"
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -47,7 +39,7 @@ export default {
     },
     BSCTest: {
       chainId: 97,
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      url: "https://data-seed-prebsc-2-s1.binance.org:8545/",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
     }
   }
---
+++ scripts/UPMintPublic.ts
@@ -0,0 +1,15 @@
+import { task } from "hardhat/config"
+
+task("UPMintPublic", "Deploy UPMintPublic contract")
+  .addParam("up", "UP Address")
+  .addParam("controller", "UPController Address")
+  .addOptionalParam("mintRate", "mintRate = 5 = 5%", "5")
+  .setAction(async ({ up, controller, mintRate }, hre) => {
+    await hre.run("compile")
+
+    const UPMintPublicContract = await hre.ethers.getContractFactory("UPMintPublic")
+    const upMintPublic = await UPMintPublicContract.deploy(up, controller, mintRate)
+    await upMintPublic.deployed()
+
+    console.log("UPMintPublic -", upMintPublic.address)
+  })
