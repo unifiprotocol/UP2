99b5c27baee39433b4c9d2806b37adb2fd5593f8
Carlos Lostao <hastro1990@gmail.com>
2022-07-06 11:43:35 +0200

    update: attributes are interfaces instead of its addresses

--- contracts/Rebalancer.sol
+++ contracts/Rebalancer.sol
@@ -20,12 +20,12 @@ contract Rebalancer is AccessControl, Pausable, Safe {
   bytes32 public constant STAKING_ROLE = keccak256("STAKING_ROLE");
 
   address public WETH = address(0);
-  address public strategy = address(0);
+  IStrategy public strategy;
   address public unifiFactory = address(0);
-  address public liquidityPool = address(0);
-  address payable public darbi = payable(address(0));
-  address payable public UPaddress = payable(address(0));
-  address payable public UP_CONTROLLER = payable(address(0));
+  IUnifiPair public liquidityPool;
+  Darbi public darbi;
+  UP public UPToken;
+  UPController public UP_CONTROLLER;
   uint256 public allocationLP = 5; //Whole Number for Percent, i.e. 5 = 5%
   uint256 public allocationRedeem = 5; //Whole Number for Percent, i.e. 5 = 5%
   uint256 public slippageTolerance = 10; //Percent with 2 Percision, i.e. 10 = 0.1%
@@ -53,22 +53,23 @@ contract Rebalancer is AccessControl, Pausable, Safe {
     address _liquidityPool,
     address _darbi
   ) {
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     WETH = _WETH;
-    UPaddress = payable(_UPAddress);
-    UP_CONTROLLER = payable(_UPController);
-    strategy = _Strategy;
+    setUPController(_UPController);
+    setStrategy(_Strategy);
+    UPToken = UP(payable(_UPAddress));
     unifiRouter = IUniswapV2Router02(_unifiRouter);
     unifiFactory = _unifiFactory;
-    liquidityPool = _liquidityPool;
-    darbi = payable(_darbi);
+    liquidityPool = IUnifiPair(payable(_liquidityPool));
+    darbi = Darbi(payable(_darbi));
   }
 
   receive() external payable {}
 
   function claimAndBurn() internal {
-    uint256 claimedUP = IUnifiPair(liquidityPool).claimUP(address(this));
-    UP(UPaddress).approve(UPaddress, claimedUP);
-    UP(UPaddress).justBurn(claimedUP);
+    uint256 claimedUP = liquidityPool.claimUP(address(this));
+    UPToken.approve(address(UPToken), claimedUP);
+    UPToken.justBurn(claimedUP);
   }
 
   function getupcBalance() internal view returns (uint256 upcBalance) {
@@ -79,25 +80,23 @@ contract Rebalancer is AccessControl, Pausable, Safe {
     // Step 1
     claimAndBurn(); 
 
-    IERC20 lp = IERC20(liquidityPool);
+    IERC20 lp = IERC20(address(liquidityPool));
     uint256 lpBalance = lp.balanceOf(address(this)); //Put in Variable Defination
 
-    UPController upController = UPController(UP_CONTROLLER); // Put in Variable Defination
-
     // Store a snapshot of the rewards
-    IStrategy.Rewards memory strategyRewards = IStrategy(strategy).checkRewards();
+    IStrategy.Rewards memory strategyRewards = strategy.checkRewards();
     saveReward(strategyRewards);
 
     // Gather the generated rewards by the strategy and send them to the UPController
     // Step 2
-    IStrategy(strategy).gather();
-    (bool successUpcTransfer, ) = UP_CONTROLLER.call{value: strategyRewards.rewardsAmount}(""); 
+    strategy.gather();
+    (bool successUpcTransfer, ) = address(UP_CONTROLLER).call{value: strategyRewards.rewardsAmount}(""); 
     require(successUpcTransfer, "FAIL_SENDING_REWARDS_TO_UPC");
     // UPController balances after get rewards
 
     // Force Arbitrage
     // Step 3
-    Darbi(darbi).arbitrage(); 
+    darbi.arbitrage(); 
 
     (uint256 amountLpUP, uint256 amountLpETH) = checkLiquidityPoolBalance();
     uint256 totalETH = amountLpETH + getupcBalance() + strategyRewards.depositedAmount;
@@ -109,23 +108,23 @@ contract Rebalancer is AccessControl, Pausable, Safe {
     if (address(UP_CONTROLLER).balance > ETHtoTake)  {
       // If UP Controller balance is greater than 5%, the rebalancer withdraws from the UP Controller to deposit into the strategy
       uint256 amountToWithdraw = address(UP_CONTROLLER).balance - ETHtoTake;
-      UPController(UP_CONTROLLER).borrowNative(amountToWithdraw, address(this));
-      IStrategy(strategy).deposit{value: amountToWithdraw}(amountToWithdraw);
+      UP_CONTROLLER.borrowNative(amountToWithdraw, address(this));
+      strategy.deposit{value: amountToWithdraw}(amountToWithdraw);
     //Step 4.2 
     } else if (address(UP_CONTROLLER).balance < ETHtoTake) {
       // If UP Controller balance is less than 5%, the rebalancer withdraws from the strategy to deposit into the UP Controller
       uint256 amountToDeposit = ETHtoTake - address(UP_CONTROLLER).balance;
-      IStrategy(strategy).withdraw(amountToDeposit);
-      UPController(UP_CONTROLLER).repay{value: amountToDeposit}(0);
+      strategy.withdraw(amountToDeposit);
+      UP_CONTROLLER.repay{value: amountToDeposit}(0);
     }
 
     // REBALANCE LP
     // Step 5
     lpBalance = lp.balanceOf(address(this));
-    uint256 backedValue = UPController(UP_CONTROLLER).getVirtualPrice() / 1e18;
+    uint256 backedValue = UP_CONTROLLER.getVirtualPrice() / 1e18;
     (uint256 reserves0, uint256 reserves1) = UniswapHelper.getReserves(
       unifiFactory,
-      UPaddress,
+      address(UPToken),
       WETH
     );
     uint256 marketValue = reserves0 / reserves1; 
@@ -145,9 +144,10 @@ contract Rebalancer is AccessControl, Pausable, Safe {
       uint256 diff = amountLpETH / (ETHtoTakeFromLP * 100);
 
       uint256 totalLpToRemove = lpBalance * (diff / 100);
-      IERC20(liquidityPool).approve(address(unifiRouter), totalLpToRemove);
+      address liquidityPoolAddress = address(liquidityPool);
+      IERC20(liquidityPoolAddress).approve(address(unifiRouter), totalLpToRemove);
       (uint256 amountToken, uint256 amountETH) = unifiRouter.removeLiquidityETH(
-        liquidityPool,
+        liquidityPoolAddress,
         totalLpToRemove,
         0,
         0,
@@ -155,18 +155,18 @@ contract Rebalancer is AccessControl, Pausable, Safe {
         block.timestamp + 20 minutes
       );
 
-      IStrategy(strategy).deposit{value: amountETH}(amountETH);
-      UPController(UP_CONTROLLER).repay{value: 0}(amountToken);
+      strategy.deposit{value: amountETH}(amountETH);
+      UP_CONTROLLER.repay{value: 0}(amountToken);
     } else if (amountLpETH < ETHtoTake) {
       // Step 6.2
       // calculate the amount of UP / native required. Withdraw native tokens from Strategy, mint the equivlent amount of synthetic UP, Deposit an amount of liquidity so that getupcBalance() = ETHtoTake.
       uint256 ETHtoAddtoLP = ((totalETH * allocationLP) / 100) - amountLpETH;
-      IStrategy(strategy).withdraw(ETHtoAddtoLP);
+      strategy.withdraw(ETHtoAddtoLP);
       uint256 lpPrice = amountLpUP / amountLpETH;
       uint256 UPtoAddtoLP = lpPrice * ETHtoAddtoLP;
-      upController.borrowUP(UPtoAddtoLP, address(this));
+      UP_CONTROLLER.borrowUP(UPtoAddtoLP, address(this));
       unifiRouter.addLiquidityETH{value: ETHtoAddtoLP}(
-        liquidityPool,
+        address(liquidityPool),
         UPtoAddtoLP,
         0,
         0,
@@ -177,12 +177,12 @@ contract Rebalancer is AccessControl, Pausable, Safe {
   }
 
   function checkLiquidityPoolBalance() public view returns (uint256, uint256) {
-    IERC20 lp = IERC20(liquidityPool);
+    IERC20 lp = IERC20(address(liquidityPool));
     uint256 lpBalance = lp.balanceOf(address(this));
     (bool success, bytes memory result) = address(unifiRouter).staticcall(
       abi.encodeWithSignature(
         "removeLiquidityETH(address,uint,uint,uint,address,uint)",
-        liquidityPool,
+        address(liquidityPool),
         lpBalance,
         0,
         0,
@@ -199,11 +199,11 @@ contract Rebalancer is AccessControl, Pausable, Safe {
   }
 
   function setUPController(address newAddress) public onlyAdmin {
-    UP_CONTROLLER = payable(newAddress);
+    UP_CONTROLLER = UPController(payable(newAddress));
   }
 
   function setStrategy(address newAddress) public onlyAdmin {
-    strategy = newAddress;
+    strategy = IStrategy(newAddress);
   }
 
   function setUnifiRouter(address newAddress) public onlyAdmin {
@@ -211,7 +211,7 @@ contract Rebalancer is AccessControl, Pausable, Safe {
   }
 
   function setDarbi(address newAddress) public onlyAdmin {
-    darbi = payable(newAddress);
+    darbi = Darbi(payable(newAddress));
   }
 
   function saveReward(IStrategy.Rewards memory reward) internal {
@@ -254,12 +254,12 @@ contract Rebalancer is AccessControl, Pausable, Safe {
     return _withdrawFundsERC20(target, tokenAddress);
   }
 
-  /// @notice Permissioned function to pause UPaddress Controller
+  /// @notice Permissioned function to pause UPToken Controller
   function pause() public onlyAdmin {
     _pause();
   }
 
-  /// @notice Permissioned function to unpause UPaddress Controller
+  /// @notice Permissioned function to unpause UPToken Controller
   function unpause() public onlyAdmin {
     _unpause();
   }
