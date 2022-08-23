54aeb79ca22f1552acd7cf734b61cfb8f1a21511
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-08-23 17:16:41 +0200

    refactor: harmony staking

--- contracts/Strategies/HarmonyStakingStrategyV2Remix.sol
+++ contracts/Strategies/HarmonyStakingStrategyV2Remix.sol
@@ -2,13 +2,11 @@
 
 pragma solidity ^0.8.4;
 
-import "./IStrategy.sol";
-import "../Helpers/Safe.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "./Interfaces/IUPController.sol";
-import "./Interfaces/IRebalancer.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./Strategy.sol";
 import "../Helpers/StakingPrecompiles.sol";
+import "../UPController.sol";
 
 // Contract Strategy must use our customized Safe.sol and OpenZeppelin's AccessControl and Pauseable Contracts.
 // Safe.sol is utilized so the DAO can retrieve any lost assets within the Strategy Contract.
@@ -20,51 +18,22 @@ import "../Helpers/StakingPrecompiles.sol";
 // AccessControl.sol is utilized so only the Rebalancer can interact with the strategy, and only the DAO can update the Rebalancer Contract.
 // Pausable.sol is utilized so that DAO can pause the strategy in an emergency.
 
-contract Strategy is IStrategy, Safe, AccessControl, Pausable, StakingPrecompiles {
-  bytes32 public constant REBALANCER_ROLE = keccak256("REBALANCER_ROLE");
-  bytes32 public constant MONITOR_ROLE = keccak256("MONITOR_ROLE");
-
+contract HarmonyStakingStrategy is Strategy, StakingPrecompiles {
   /// @notice The amountStaked MUST reflect the amount of native tokens currently deposited into other contracts. All deposits and withdraws so update this variable.
-  uint256 public amountStaked = 0;
   uint256 public epochOfLastRebalance = 0;
+  uint256 public amountStaked = 0;
   uint256 public lastClaimedAmount = 0;
   address public targetValidator;
-  address public monitor;
 
-  IUPController public upController;
-  IRebalancer public rebalancer;
-
-  modifier onlyAdmin() {
-    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "ONLY_ADMIN");
-    _;
-  }
-
-  modifier onlyRebalancer() {
-    require(hasRole(REBALANCER_ROLE, msg.sender), "ONLY_REBALANCER");
-    _;
-  }
-
-  modifier onlyMonitor() {
-    require(hasRole(MONITOR_ROLE, msg.sender), "ONLY_MONITOR");
-    _;
-  }
-
-  event UpdateRebalancer(address _rebalancer);
+  UPController public upController;
 
   constructor(
     address _fundsTarget,
     address _targetValidator,
-    address _upController,
-    address _rebalancer,
-    address _monitor
-  ) Safe(_fundsTarget) {
-    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    _setupRole(REBALANCER_ROLE, msg.sender);
-    _setupRole(MONITOR_ROLE, msg.sender);
+    address _upController
+  ) Strategy(_fundsTarget) {
+    upController = UPController(payable(_upController));
     targetValidator = _targetValidator;
-    upController = IUPController(payable(_upController));
-    rebalancer = IRebalancer(payable(_rebalancer));
-    monitor = _monitor;
   }
 
   // Read Functions
@@ -74,7 +43,7 @@ contract Strategy is IStrategy, Safe, AccessControl, Pausable, StakingPrecompile
   ///the function should calculate the return for selling 2 TokenA, and add to the 5 Native Tokens.
   ///If we assume that each TokenA is worth 4 native tokens, then the unclaimedEarnings value should return a value of 13, adjusted for percision.
 
-  function checkAllocation() public view virtual returns (uint256 allocationOthers) {
+  function checkAllocation() public pure returns (uint256 allocationOthers) {
     return 10;
   }
 
@@ -87,7 +56,7 @@ contract Strategy is IStrategy, Safe, AccessControl, Pausable, StakingPrecompile
     return result;
   }
 
-  function checkRewards() public view virtual override returns (IStrategy.Rewards memory) {
+  function checkRewards() public view override returns (IStrategy.Rewards memory) {
     IStrategy.Rewards memory result = IStrategy.Rewards(
       lastClaimedAmount,
       amountStaked,
@@ -97,39 +66,7 @@ contract Strategy is IStrategy, Safe, AccessControl, Pausable, StakingPrecompile
   }
 
   // Write Functions
-
-  function acceptMoney() public payable {}
-
-  function deposit(uint256 depositValue)
-    public
-    payable
-    override
-    onlyRebalancer
-    whenNotPaused
-    returns (bool)
-  {
-    require(depositValue == msg.value, "Deposit Value Parameter does not equal payable amount");
-    acceptMoney();
-    return true;
-  }
-
-  function withdraw(uint256 withdrawAmount)
-    public
-    override
-    onlyRebalancer
-    whenNotPaused
-    returns (bool)
-  {
-    require(
-      withdrawAmount <= address(this).balance,
-      "Amount Requested to Withdraw is Greater Than Currently Available in Wallet"
-    );
-    (bool successTransfer, ) = address(msg.sender).call{value: withdrawAmount}("");
-    require(successTransfer, "FAIL_SENDING_NATIVE");
-    return true;
-  }
-
-  function adjustDelegation() public onlyMonitor whenNotPaused returns (bool) {
+  function adjustDelegation() public onlyAdmin whenNotPaused returns (bool) {
     uint256 currentAllocation = checkAllocation();
     require(currentAllocation <= 100, "Allocation for LP and Redeem exceeds 100%");
     uint256 targetAmountToStake = (200000000000000000000 * (100 - currentAllocation)) / 100;
@@ -155,23 +92,11 @@ contract Strategy is IStrategy, Safe, AccessControl, Pausable, StakingPrecompile
     return true;
   }
 
-  ///@notice The withdrawAll function should withdraw all native tokens, including rewards as native tokens, and send them to the rebalancer.
-
-  function withdrawAll() external virtual override onlyRebalancer whenNotPaused returns (bool) {
-    // Check if any native tokens are still delegated
-    // If so, undelegate
-    // Check if amountDeposited < 1
-    // If not, undelegate. Return message that native tokens are still being undelegated. Rebalance must occur after undelegation funds are received.
-    (bool successTransfer, ) = address(msg.sender).call{value: address(this).balance}("");
-    require(successTransfer, "FAIL_SENDING_NATIVE");
-    return true;
-  }
-
   ///@notice The gather function should claim all yield earned from the native tokens while leaving the amount deposited intact.
   ///Then, this function should send all earnings to the rebalancer contract. This function MUST only send native tokens to the rebalancer.
   ///For example, if the tokens are deposited in a lending protocol, it should the totalBalance minus the amountDeposited.
 
-  function gather() public virtual override onlyRebalancer whenNotPaused {
+  function gather() public override onlyRebalancer whenNotPaused {
     uint256 currentEpoch = epoch();
     require(
       currentEpoch > epochOfLastRebalance + 7,
@@ -187,13 +112,5 @@ contract Strategy is IStrategy, Safe, AccessControl, Pausable, StakingPrecompile
     (bool successTransfer, ) = address(msg.sender).call{value: claimedRewards}("");
     require(successTransfer, "FAIL_SENDING_NATIVE");
     epochOfLastRebalance = currentEpoch;
-  }
-
-  function withdrawFunds() public onlyAdmin returns (bool) {
-    return _withdrawFunds();
-  }
-
-  function withdrawFundsERC20(address tokenAddress) public onlyAdmin returns (bool) {
-    return _withdrawFundsERC20(tokenAddress);
   }
 }
--- contracts/Strategies/Interfaces/IRebalancer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IRebalancer {
-    function allocationLP() external view returns (uint256);
-    function allocationRedeem() external view returns (uint256);
-}
